e5fe9c1cd60fcd3aa13bbe69c365d106f7153758
dark-glich <mohdbilal9808464636@gmail.com>
2021-07-01 13:35:20 +0530

    Completed the basics of pandas

--- Practice/Main.py
+++ Practice/Main.py
@@ -26,6 +26,23 @@ print(csv[['Name', 'Attack', 'Defense']])
 print(csv.iloc[200])
 print(csv.iloc[30:35])
 
-# Reading a specific cell
+# Reading a specific cell 
 print(csv.iloc[2,3]) # csv.iloc[column, row]
 
+# Getting a conditional cells
+print(csv.loc[csv['HP'] > 100]) #prints all cells whose hp is greater than 100
+
+# Getting mathematical info from data
+print(csv.describe())
+
+# Sorting Data 
+print(csv.sort_values('Type 1')) # sortes data according to Type 1
+print(csv.sort_values(['Type 1', 'HP']))
+
+# Creating a column
+csv['Total'] = csv['Attack'] + csv['Defense']
+print(csv.head(5))
+
+# Deleting a column from Data
+csv = csv.drop(columns=['Type 2'])
+print(csv.head(3))
