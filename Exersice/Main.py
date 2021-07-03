2706702b921e4d03b8f1318c26d0880cf80b6959
dark-glich <mohdbilal9808464636@gmail.com>
2021-07-03 14:13:30 +0530

    completed basic functions on data

--- Exersice/Main.py
+++ Exersice/Main.py
@@ -1,6 +1,7 @@
 import pandas as pd 
 import os
 
+
 # Merging 12 months sales data into single csv file
 files = [file for file in os.listdir('Exersice/Data/Input')]
 data = pd.DataFrame()
@@ -9,3 +10,21 @@ for file in files:
     data = pd.concat([data, df])
     
 data.to_csv('Exersice/Data/Output/all_data.csv')
+
+# Remove row with Nan value
+data.dropna(subset=['Order Date'], inplace=True)
+
+# Removing other non integer rows
+data = data[data['Order Date'].str[0:2] != 'Or']
+
+# Creating a month column
+data['Month'] = data['Order Date'].str[0:2]
+data['Month'] = data['Month'].astype('int16')
+
+# Creating a total column
+data['Price Each'] =  data['Price Each'].astype('float32')
+data['Quantity Ordered'] = data['Quantity Ordered'].astype('int32')
+data['Total'] = data['Price Each'] * data['Quantity Ordered']
+
+# Which month has best sales
+print(data.groupby('Month').sum())
